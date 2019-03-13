4788fbe80644c86610069b64714dbe70be37e471
Denni2 <wanjofu26@live.com>
2019-03-13 17:50:58 +0300

    new changes to the overall project

--- javascript/main.js
+++ javascript/main.js
@@ -1,112 +1,147 @@
-var myFullpage = new fullpage('#content', {
-    //Navigation
-    menu: '#menu',
-    lockAnchors: false,
-    anchors: ['firstPage', 'secondPage'],
-    navigation: true,
-    navigationPosition: 'right',
-    navigationTooltips: ['firstSlide', 'secondSlide'],
-    showActiveTooltip: false,
-    slidesNavigation: false,
-    slidesNavPosition: 'bottom',
+//refresh all pages on reload
 
-    //Scrolling
-    css3: true,
-    scrollingSpeed: 1500,
-    autoScrolling: true,
-    fitToSection: true,
-    fitToSectionDelay: 2000,
-    scrollBar: false,
-    easing: 'easeInOutCubic',
-    easingcss3: 'ease',
-    loopBottom: false,
-    loopTop: false,
-    loopHorizontal: true,
-    continuousVertical: false,
-    continuousHorizontal: false,
-    scrollHorizontally: false,
-    interlockedSlides: false,
-    dragAndMove: false,
-    offsetSections: false,
-    resetSliders: false,
-    fadingEffect: true,
-    normalScrollElements: '#element1, .element2',
-    scrollOverflow: false,
-    scrollOverflowReset: false,
-    scrollOverflowOptions: null,
-    touchSensitivity: 15,
-    normalScrollElementTouchThreshold: 5,
-    bigSectionsDestination: null,
-
-    //Accessibility
-    keyboardScrolling: true,
-    animateAnchor: true,
-    recordHistory: true,
-
-    //Design
-    controlArrows: true,
-    verticalCentered: true,
-    paddingTop: '3em',
-    paddingBottom: '10px',
-    fixedElements: '#header, .footer',
-    responsiveWidth: 0,
-    responsiveHeight: 0,
-    responsiveSlides: false,
-    parallax: false,
-    parallaxOptions: { type: 'reveal', percentage: 62, property: 'translate' },
-
-    //Custom selectors
-    sectionSelector: '.section',
-    slideSelector: '.slide',
-
-    lazyLoading: true
+window.addEventListener('load', function () {
+    Barba.Pjax.start();    
 });
 
-/*
-function animateHome (){
-}
+var FadeTransition = Barba.BaseTransition.extend({
+    start: function () {
+        /**
+         * This function is automatically called as soon the Transition starts
+         * this.newContainerLoading is a Promise for the loading of the new container
+         * (Barba.js also comes with an handy Promise polyfill!)
+         */
 
+        // As soon the loading is finished and the old page is faded out, let's fade the new page
+        Promise
+            .all([this.newContainerLoading, this.fadeOut()])
+            .then(this.fadeIn.bind(this));
+    },
 
-/*
-function hoverMouse(id) {
-    let val = document.getElementById(id);
-    val.className = val.className.replace(' rounders', ' heart');
-}
-function clearMouse(id){
-    let val = document.getElementById(id);
-    val.className = val.className.replace(' heart', ' rounders');
-}
-*/
-/*
- * the below code caters for the map in the code
- * and the timer that is yet to be implemented
- *
+    fadeOut: function () {
+        /**
+         * this.oldContainer is the HTMLElement of the old Container
+         */
 
-var day_zero = new Date ("November 10, 2018 10:30:00");
+        return $(this.oldContainer).animate({ opacity: 0 }).promise();
+    },
 
-var countUpdate = setInterval(function () {
-    var now = new Date().getTime();
+    fadeIn: function () {
+        /**
+         * this.newContainer is the HTMLElement of the new Container
+         * At this stage newContainer is on the DOM (inside our #barba-container and with visibility: hidden)
+         * Please note, newContainer is available just after newContainerLoading is resolved!
+         */
 
-    //get the difference between today and then
+        var _this = this;
+        var $el = $(this.newContainer);
 
-    var daysLeft = day_zero - now;
+        $(this.oldContainer).hide();
 
-    // calculations
-    var days = Math.floor(daysLeft / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((daysLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((daysLeft % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((daysLeft % (1000 * 60)) / 1000);
+        $el.css({
+            visibility: 'visible',
+            opacity: 0
+        });
 
-    // output
+        $el.animate({ opacity: 1 }, 400, function () {
+            /**
+             * Do not forget to call .done() as soon your transition is finished!
+             * .done() will automatically remove from the DOM the old Container
+             */
 
-    //document.getElementById("countdown").innerHTML = ();
-
-    // actions after countdown complete
-    /*if (daysLeft <= 0){
-        document.getElementById("countdown").innerHTML = ("WEDDING DAY!!!!!!");
-    }*
-    
+            _this.done();
+        });
+    }
 });
+
+/**
+ * Next step, you have to tell Barba to use the new Transition
+ */
+
+Barba.Pjax.getTransition = function () {
+    /**
+     * Here you can use your own logic!
+     * For example you can use different Transition based on the current page or link...
+     */
+
+    return FadeTransition;
+};
+
+
+//gallery area
+
+
+let innerId = document.getElementById('img1');
+getImg(innerId);
+
+
+// wedding area 
+
+let first = document.getElementById("first");
+let second = document.getElementById("second");
+let third = document.getElementById("third");
+let fourth = document.getElementById("fourth");
+let fifth = document.getElementById("fifth");
+let sixth = document.getElementById('sixth');
+
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let three = document.getElementById("three");
+let four = document.getElementById("four");
+let five = document.getElementById("five");
+let six = document.getElementById("six");
+
+var span;
+
+function mouseHover(new_val) {
+    new_val.addEventListener('mouseenter', function () {
+        if (new_val.classList == 'info-bubble') {
+            new_val.classList.remove('info-bubble');
+            new_val.classList.add("info-bubbled");
+        }
+    });
+    new_val.addEventListener('mouseleave', function () {
+        if (new_val.className == 'info-bubbled') {
+            new_val.classList.remove('info-bubbled');
+            new_val.classList.add("info-bubble");
+        }
+    });
+}
+mouseHover(first);
+mouseHover(second);
+mouseHover(third);
+mouseHover(fourth);
+mouseHover(fifth);
+mouseHover(sixth);
+
+// insert modals 
+
+function clicker(vals, mod_val){
+    vals.onclick = function () {
+        mod_val.style.display = 'block';    
+    }
+    span = mod_val;
+    span.onclick = function () {
+        mod_val.style.display = 'none';
+    }
+    window.onclick = function (event) {
+        if (event.target == mod_val){
+            mod_val.style.display = 'none';
+        }
+    }
+}
+
+// images
+//clicker(im1, img1);
+
+
+// wedding ideas
+clicker(first, one);
+clicker(second, two);
+clicker(third, three);
+clicker(fourth, four);
+clicker(fifth, five);
+clicker(sixth, six);
 
 //LatLng(-1.2299108, 36.853296)
 // my api key: - AIzaSyAANZDGmMfdy8mH-s38Ylm08hO5fU08AA4
@@ -125,4 +160,3 @@ function initMap (){
         title: 'Ol Arabel'
     });
 }
-*/
